729f9525bb35debda6ec5d14fbcc70bbaae34700
Santhosh06-DV <santhoshdv06@gmail.com>
2025-10-24 12:17:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload Sailes having 2 or 4 boyfriends.
         </p>
         <a
           className="App-link"
